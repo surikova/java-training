d66b44561151794c07540b7ad248309cc0aa745b
Kateryna Surikova <surikova.cat@gmail.com>
2017-01-14 22:28:22 +0100

    first task

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Created by kate on 14.01.17.
+ */
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, Kate !");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastNumber = 1000 - myTotal;
+
+        System.out.println(myLastNumber);
+    }
+}
