208e0f415acf37cc3053ae6cc4190646bc625946
Kateryna Surikova <surikova.cat@gmail.com>
2017-01-15 20:19:09 +0100

    int as string, questions in the 2 file

---
+++ src/intAsString.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kate on 15.01.17.
+ */
+public class intAsString {
+    public static void main(String[] arg) {
+        int b = 12;
+        System.out.println(String.valueOf(b));
+
+    }
+}
---
+++ src/intAsString2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kate on 15.01.17.
+ */
+public class intAsString2 {
+    private String getIntAsString(int someInt){
+        someInt = 13;
+        System.out.println(String.valueOf(someInt));
+    }
+}
