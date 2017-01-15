8779f0bdbccca67398ffa1f9e2268ad3a1b6cd87
Kateryna Surikova <surikova.cat@gmail.com>
2017-01-15 19:19:52 +0100

    float and double

---
+++ src/floatAndDouble.java
@@ -0,0 +1,22 @@
+/**
+ * Created by kate on 15.01.17.
+ */
+public class floatAndDouble {
+    public static void main(String[] args) {
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+
+        double pounds = 13.3d;
+        double poundsToKilos = pounds * 0.45359237d;
+        System.out.println("poundsToKilos = " + poundsToKilos);
+
+
+
+    }
+}
