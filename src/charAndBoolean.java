1fce26a05e24d2a6c16d84affc4e5ad676d53948
Kateryna Surikova <surikova.cat@gmail.com>
2017-01-15 19:32:42 +0100

    char and boolean

---
+++ src/charAndBoolean.java
@@ -0,0 +1,16 @@
+/**
+ * Created by kate on 15.01.17.
+ */
+public class charAndBoolean {
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char mySecondChar = '\u00A3';
+        System.out.println("Unicode output was: " + mySecondChar);
+
+    }
+}
