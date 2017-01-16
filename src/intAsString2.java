97ad57da7875b8050d186698bab31c215dce2e92
Kateryna Surikova <surikova.cat@gmail.com>
2017-01-16 23:50:37 +0100

    convert method to void

--- src/intAsString2.java
+++ src/intAsString2.java
@@ -2,8 +2,13 @@
  * Created by kate on 15.01.17.
  */
 public class intAsString2 {
-    private String getIntAsString(int someInt){
-        someInt = 13;
+    public static void main(String[] args) {
+        getIntAsString(2);
+        getIntAsString(13);
+        getIntAsString(4489003);
+    }
+
+    private static void getIntAsString(int someInt){
         System.out.println(String.valueOf(someInt));
     }
 }
